2d793934310f2c014a942600628bdae40ce5e0b8
Rishikant Badkur <badkurrishi@gmail.com>
2022-12-12 12:02:28 +0530

    final considerations

--- utils/email.js
+++ utils/email.js
@@ -12,7 +12,7 @@ module.exports = class Email {
 
   newTransport() {
     if (process.env.NODE_ENV === 'production') {
-      console.log('prod mode');
+      // console.log('prod mode');
       return nodemailer.createTransport({
         service: 'SendGrid',
         auth: {
@@ -67,35 +67,4 @@ console.log('dev mode');
   }
 };
 
-/* const sendEmail = async (options) => {
-  //1 Create a transporter
-  // Transporter in gmail
-  /*const transporter = nodemailer.createTransport({
-    service: 'Gmail',
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // Activate less secure app in gmail.
 
-  const transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-  // 2 Define the email options
-  const mailOptions = {
-    from: 'Rishikant Badkur <rishi01badkur.io>',
-    to: options.email,
-    subject: options.subject,
-    text: options.message,
-  };
-  // 3 Actually send the email
-  await transporter.sendMail(mailOptions);
-};
-
-module.exports = sendEmail;*/
